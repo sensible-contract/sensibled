e1fc84c3ab174bcb85e797f407492a214a778853
jiedo <jiedohh@gmail.com>
2021-05-30 21:15:41 +0800

    feat: remove dump log conf

--- logger/logger.go
+++ logger/logger.go
@@ -11,17 +11,10 @@ import (
 var (
 	Log    *zap.Logger
 	LogErr *zap.Logger
-
-	LogBlk      *zap.Logger
-	LogTx       *zap.Logger
-	LogTxIn     *zap.Logger
-	LogTxInFull *zap.Logger
-	LogTxOut    *zap.Logger
-	DEBUG       = true
 )
 
 func init() {
-	viper.SetConfigFile("conf/dump.yaml")
+	viper.SetConfigFile("conf/log.yaml")
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
 			panic(fmt.Errorf("Fatal error config file: %s \n", err))
@@ -30,10 +23,7 @@ func init() {
 		}
 	}
 
-	dumpEncoding := viper.GetString("encoding")
 	logFile := viper.GetString("logFile")
-	pathPrefix := viper.GetString("pathPrefix")
-	pathSurfix := viper.GetString("pathSurfix")
 
 	zap.RegisterEncoder("row-binary", constructRowBinaryEncoder)
 	zap.RegisterEncoder("row-binary-debug", constructRowBinaryEncoderDebug)
@@ -53,46 +43,9 @@ func init() {
 		DisableStacktrace: true,
 		OutputPaths:       []string{"stderr"},
 	}.Build()
-
-	LogBlk, _ = zap.Config{
-		Encoding:          dumpEncoding,
-		Level:             zap.NewAtomicLevelAt(zapcore.InfoLevel),
-		DisableCaller:     true,
-		DisableStacktrace: true,
-		OutputPaths:       []string{pathPrefix + "/blk" + pathSurfix},
-	}.Build()
-
-	LogTx, _ = zap.Config{
-		Encoding:          dumpEncoding,
-		Level:             zap.NewAtomicLevelAt(zapcore.InfoLevel),
-		DisableCaller:     true,
-		DisableStacktrace: true,
-		OutputPaths:       []string{pathPrefix + "/tx" + pathSurfix},
-	}.Build()
-
-	LogTxIn, _ = zap.Config{
-		Encoding:          dumpEncoding,
-		Level:             zap.NewAtomicLevelAt(zapcore.InfoLevel),
-		DisableCaller:     true,
-		DisableStacktrace: true,
-		OutputPaths:       []string{pathPrefix + "/txin" + pathSurfix},
-	}.Build()
-
-	LogTxOut, _ = zap.Config{
-		Encoding:          dumpEncoding,
-		Level:             zap.NewAtomicLevelAt(zapcore.InfoLevel),
-		DisableCaller:     true,
-		DisableStacktrace: true,
-		OutputPaths:       []string{pathPrefix + "/txout" + pathSurfix},
-	}.Build()
-
 }
 
 func SyncLog() {
 	Log.Sync()
 	LogErr.Sync()
-	LogBlk.Sync()
-	LogTx.Sync()
-	LogTxIn.Sync()
-	LogTxOut.Sync()
 }
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-LISTEN=0.0.0.0:8000 nohup ./satoblock -map -sync >> stdout.log 2>> stderr.log &
+#nohup ./satoblock >> stdout.log 2>> stderr.log &
+./satoblock
