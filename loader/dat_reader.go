780acd0b0ddc5124743f1322aa46782f95346ac3
jiedo <jiedohh@gmail.com>
2022-07-06 15:30:21 +0800

    inner function

--- loader/dat_reader.go
+++ loader/dat_reader.go
@@ -33,18 +33,18 @@ func NewBlockData(path string, magic []byte) (bf *BlockData) {
 
 // GetRawBlockHeader
 func (bf *BlockData) GetRawBlockHeader() (rawblockheader []byte, err error) {
-	return bf.NextRawBlockData(true)
+	return bf.nextRawBlockData(true)
 }
 
 func (bf *BlockData) GetRawBlock() (rawblock []byte, err error) {
-	return bf.NextRawBlockData(false)
+	return bf.nextRawBlockData(false)
 }
 
-func (bf *BlockData) NextRawBlockData(skipTxs bool) (rawblock []byte, err error) {
+func (bf *BlockData) nextRawBlockData(skipTxs bool) (rawblock []byte, err error) {
 	bf.m.Lock()
 	defer bf.m.Unlock()
 
-	rawblock, err = bf.FetchNextBlock(skipTxs)
+	rawblock, err = bf.fetchNextBlock(skipTxs)
 	if err != nil {
 		newblkfile, err2 := os.Open(blkfilename(bf.Path, bf.CurrentId+1))
 		if err2 != nil {
@@ -55,25 +55,25 @@ func (bf *BlockData) NextRawBlockData(skipTxs bool) (rawblock []byte, err error)
 		bf.CurrentFile.Close()
 		bf.CurrentFile = newblkfile
 		bf.Offset = 0
-		rawblock, err = bf.FetchNextBlock(skipTxs)
+		rawblock, err = bf.fetchNextBlock(skipTxs)
 	}
 	return rawblock, nil
 }
 
-func (bf *BlockData) FetchNextBlock(skipTxs bool) (rawblock []byte, err error) {
+func (bf *BlockData) fetchNextBlock(skipTxs bool) (rawblock []byte, err error) {
 	bf.LastOffset = bf.Offset
 
 	buf := [4]byte{}
 	_, err = bf.CurrentFile.Read(buf[:])
 	if err != nil {
-		// logger.Log.Info("FetchNextBlock done", zap.Error(err))
+		// logger.Log.Info("fetchNextBlock done", zap.Error(err))
 		return
 	}
 	bf.Offset += 4
 
 	if !bytes.Equal(buf[:], bf.Magic) {
 		err = errors.New("Bad magic")
-		logger.Log.Info("FetchNextBlock done: Bad magic",
+		logger.Log.Info("fetchNextBlock done: Bad magic",
 			zap.Int("fid", bf.CurrentId),
 			zap.Int("offset", bf.Offset),
 			zap.String("buf", hex.EncodeToString(buf[:])),
